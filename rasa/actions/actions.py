7ca750a6b17d3beb464e5bac3b79814f565609ea
Eduard <eduvives2212@gmail.com>
2022-09-28 22:52:32 +0200

    CRSF protection parameters sent to Rasa and Custom Action created
    - Added the functions to obtain the variables related to Django's Cross Site Request Forgery protection.
    - Protection variables sent to the Rasa Server via the initPayload attribute of the chat widget.
    - Created a Custom Action that reads the protection variables and calls the corresponding endpoint assigning the protection cookies and parameters to the request.
    - Added the chat widget to display the virtual assistant to the main page template (index.html), for testing.

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -7,21 +7,60 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
+from typing import Any, Text, Dict, List
+
+from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+import requests
+domainUrl = "http://localhost:8000/"
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+# class storeSessionSlots(Action):
 #
 #     def name(self) -> Text:
-#         return "action_hello_world"
+#         return "store_session_slots"
 #
 #     def run(self, dispatcher: CollectingDispatcher,
 #             tracker: Tracker,
 #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 #
-#         dispatcher.utter_message(text="Hello World!")
+#         print(tracker.latest_message['entities'])
 #
-#         return []
+#         return [SlotSet("csrftoken", "text2"),SlotSet("csrfmiddlewaretoken", "text3")]
+
+class ActionLogout(Action):
+
+    def name(self) -> Text:
+        return "action_logout"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        requests.get(domainUrl + "selected_data/", params={"csrfmiddlewaretoken":tracker.get_slot("csrfmiddlewaretoken")},
+                     cookies={"sessionid":tracker.get_slot("sessionid"),"csrftoken":tracker.get_slot("csrftoken")})
+
+        if (tracker.get_slot("sessionid")):
+            print("yes sessionid")
+        else:
+            print("no sessionid")
+        if (requests): # successful response
+            dispatcher.utter_message(text="The session has been successfully closed")
+        else:
+            dispatcher.utter_message(text="An error has occurred")
+
+        return []
