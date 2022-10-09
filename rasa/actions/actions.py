67bd1eddc867e934cd560834488e092b4a7cc70a
Eduard <eduvives2212@gmail.com>
2022-10-09 12:50:00 +0200

    Added Custom Action Validation in the Rasa "Signup Form"
    - When attempting to register a new user via the chatbot, both username and password information is validated, according to Django's requirements and standards. The user is immediately informed if any requirement is not met, allowing the user to re-enter a valid value.

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -10,9 +10,11 @@
 from typing import Any, Text, Dict, List
 
 from rasa_sdk.events import SlotSet, FollowupAction
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
 
+import re
 import requests
 
 domainUrl = "http://localhost:8000/"
@@ -139,7 +141,7 @@ class ActionLogoutToSignupCancellation(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text="I will not log you out, but the new user will not register")
+        dispatcher.utter_message(text="Okey! I will not log you out")
         return [SlotSet("username", None),SlotSet("password", None),SlotSet("password_confirmation", None)]
 
 # class ActionLogin(Action):
@@ -258,6 +260,72 @@ class ActionSignup(Action):
                                       + ',' + encrypt(tracker.get_slot("password_confirmation").encode(), key).decode())
         return [SlotSet("username", None), SlotSet("password", None), SlotSet("password_confirmation", None)]
 
+class ValidateSignupForm(FormValidationAction):
+
+    def name(self) -> Text:
+        return "validate_signup_form"
+
+    def validate_username(self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate username value."""
+
+        # make a pattern
+        pattern = "^[A-Za-z0-9@.+-_]*$"
+
+        if len(slot_value) > 150:
+            dispatcher.utter_message(text="Ensure this value has at most 150 characters (it has " + str(len(slot_value)) + ")")
+        elif not re.match(pattern, slot_value):
+            dispatcher.utter_message(text="Enter a valid username. This value may contain only letters, numbers, and @/./+/-/_ characters.")
+        else:
+            # validation succeeded, set the value of the "username" slot to value
+            return {"username": slot_value}
+
+        # validation failed, set this slot to None so that the
+        # user will be asked for the slot again
+        return {"username": None}
+
+    @staticmethod
+    def common_passwords_db() -> List[Text]:
+        """Database of common passwords"""
+
+        return ["12345", "123456", "1234567", "12345678", "123456789", "qwerty", "password", "123123", "123123123",
+                "1234567890", "qwerty123", "password1", "football", "monkey", "baseball", "dragon"]
+
+    def validate_password(self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate password value."""
+
+        succeed = True
+
+        if slot_value == tracker.get_slot("username"):
+            dispatcher.utter_message(text="The password is too similar to the username.")
+            succeed = False
+        if len(slot_value) < 8:
+            dispatcher.utter_message(text="This password is too short. It must contain at least 8 characters.")
+            succeed = False
+        # Check if the password is within a dictionary of common passwords
+        # or if the password consists of the same character repeated multiple times
+        if slot_value.lower() in self.common_passwords_db() or slot_value.lower() == len(slot_value) * slot_value[0].lower():
+            dispatcher.utter_message(text="This password is too common.")
+            succeed = False
+        if slot_value.isdigit():
+            dispatcher.utter_message(text="This password is entirely numeric.")
+            succeed = False
+        if succeed:
+            # validation succeeded, set the value of the "password" slot to value
+            return {"password": slot_value}
+        else:
+            # validation failed, set this slot to None so that the
+            # user will be asked for the slot again
+            return {"password": None}
 
 class ActionOpenDocument(Action):
 
