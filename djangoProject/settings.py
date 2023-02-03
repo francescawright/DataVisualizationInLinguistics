40a816cbc9110c7bb18e2cd83f567fa3ac826cfa
Ecem <ekavazka27@alumnes.ub.edu>
2023-02-03 19:27:43 +0100

    database

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': '',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': 5432,
     }
 
